6ff946bcefeabab8ee266ca9c7aeca8f9e1f04bc
Nayan Sharma <nayan@Nayans-MacBook-Air.local>
2025-01-19 02:15:59 +0530

    Add correct regex for youtube url check and return response for streams route

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import { prismaClient } from "@/app/lib/db";
-const YT_REGEX = new RegExp("^https://www.youtube.com/watch?v=[w-]{11}$");
+const YT_REGEX =
+  /^(?:(?:https?:)?\/\/)?(?:www\.)?(?:m\.)?(?:youtu(?:be)?\.com\/(?:v\/|embed\/|watch(?:\/|\?v=))|youtu\.be\/)((?:\w|-){11})(?:\S+)?$/;
 const ST_REGEX = new RegExp("^https://open.spotify.com/track/[w-]{22}?si=[w]{16}");
 
 const StreamSchema = z.object({
@@ -11,12 +12,14 @@ const StreamSchema = z.object({
 });
 
 const Post = async (req: NextRequest) => {
+  console.log("Nayan");
   try {
     // extract data from request
     const data = StreamSchema.parse(await req.json());
+    console.log(data);
 
     // validate url with regex
-    const isYt = YT_REGEX.test(data.url);
+    const isYt = data.url.match(YT_REGEX);
     const isSp = ST_REGEX.test(data.url);
     if (!isYt && !isSp) {
       return NextResponse.json({ message: "Invalid URL format" }, { status: 411 });
@@ -25,7 +28,7 @@ const Post = async (req: NextRequest) => {
     // fetch id (everything after ?v=) from youtube url
     const extractedId = data.url.split("?v=")[1];
 
-    await prismaClient.stream.create({
+    const stream = await prismaClient.stream.create({
       data: {
         userId: data.creatorId,
         url: data.url,
@@ -33,6 +36,11 @@ const Post = async (req: NextRequest) => {
         type: data.type,
       },
     });
+
+    return NextResponse.json(
+      { message: "Added stream successfully", id: stream.id },
+      { status: 201 }
+    );
   } catch (e) {
     return NextResponse.json({ message: "Error while adding a stream" }, { status: 411 });
   }
@@ -52,4 +60,4 @@ const Get = async (req: NextRequest) => {
   return NextResponse.json(streams);
 };
 
-export default { Post, Get };
+export { Post as POST, Get as GET };
